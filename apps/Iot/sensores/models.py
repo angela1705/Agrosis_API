b3c5d2a53f3aabfad4eefc0d0d098ec0bee915f3
DavidJuan2810 <Juandavides120@gmail.com>
2024-12-03 09:00:27 -0500

    Modelo de Sensores creado

--- apps/Iot/sensores/models.py
+++ apps/Iot/sensores/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from apps.Iot.datos_meteorologicos.models import Datos_metereologicos
 
 # Create your models here.
+class Sensores(models.Model):
+    fk_datos_metereologicos = models.ForeignKey(Datos_metereologicos,on_delete=models.SET_NULL,null=True)
+    tipo_sensores = models.CharField(max_length=200)
+    def __str__(self) -> str:
+        return "Sensor de tipo: " + str(self.tipo_sensores)
