e478b5dedee635c2c7599a557d9665f37f0240a3
DavidJuan2810 <Juandavides120@gmail.com>
2024-12-03 09:21:17 -0500

    Datos_metereologicos ViewSet creado

---
+++ apps/Iot/datos_meteorologicos/api/views.py
@@ -0,0 +1,9 @@
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from ..models import Datos_metereologicos
+from .serializers import Datos_metereologicosSerializer
+
+class ConfiguracionesViewset(ModelViewSet):
+    queryset = Datos_metereologicos.objects.all()
+    serializer_class = Datos_metereologicosSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
