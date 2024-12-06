c2cbdfd828d80ebafeab246857f7264c1f458af2
Mauricio <mauricioaudor15@gmail.com>
2024-12-05 22:20:06 -0500

    Se agregaron los modelos, serializers, views y routers a las apps que faltaban las cuales son semilleroInsumo semilleroHerramienta y tareas, tambien se agrego a las urls los routers de los modulos de cultivo, inventario e iot para evitar conflictos

--- apps/Cultivo/semillero_herramienta/models.py
+++ apps/Cultivo/semillero_herramienta/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class SemilleroHerramienta(models.Model):
+    semilla = models.ForeignKey('semillero.Semillero', on_delete=models.CASCADE)
+    herramienta = models.ForeignKey('herramientas.Herramienta', on_delete=models.CASCADE)  
+    cantidad_necesaria = models.FloatField()
+    bodega = models.ForeignKey('bodega.Bodega', on_delete=models.CASCADE)  
+
+    def __str__(self):
+        return f"Semillero: {self.semilla} - Herramienta: {self.herramienta}"
