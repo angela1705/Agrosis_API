3dd8e09b618c9c354edf7b14871259c42eac00fa
David <Juandavides120@gmail.com>
2025-02-20 18:01:29 -0500

    Reinstalacion o descomentacion  de permisos en las vistas en la carpeta api de cada carpeta de IOT

--- apps/Iot/datos_meteorologicos/api/views.py
+++ apps/Iot/datos_meteorologicos/api/views.py
@@ -3,10 +3,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from ..models import Datos_metereologicos
 from .serializers import Datos_metereologicosSerializer
-#from apps.Usuarios.usuarios.api.permissions import IsAdminOrRead
+from apps.Usuarios.usuarios.api.permissions import IsAdminOrRead
 
 class Datos_metereologicosViewset(ModelViewSet):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated ]
+    permission_classes = [IsAuthenticated,IsAdminOrRead ]
     queryset = Datos_metereologicos.objects.all()
     serializer_class = Datos_metereologicosSerializer
