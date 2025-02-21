8ae8cfb8d42c925d8aceedd0ec756f2e186efd27
WMauricioDev <oscar_audor@soy.sena.edu.co>
2025-02-20 20:58:52 -0500

    se corrigio el asgi para manejar los dos websockets, y en las urls un slash que faltaba y una coma

--- Agrosoft/asgi.py
+++ Agrosoft/asgi.py
@@ -1,18 +1,21 @@
 import os
 import django
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import apps.Cultivo.actividades.api.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Agrosoft.settings')
 django.setup()
 
+from apps.Iot.sensores.api.routers import websocket_urlpatterns as sensores_ws
+from apps.Cultivo.actividades.api.routing import websocket_urlpatterns as actividades_ws
+
+combined_ws_urlpatterns = sensores_ws + actividades_ws
+
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            apps.Cultivo.actividades.api.routing.websocket_urlpatterns
-        )
+        URLRouter(combined_ws_urlpatterns)
     ),
 })
--- Agrosoft/urls.py
+++ Agrosoft/urls.py
@@ -91,7 +91,7 @@ urlpatterns = [
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/', include(router.urls)),
-    path('cultivo', include(routerCultivo.urls))
+    path('cultivo/', include(routerCultivo.urls)),
     path('', include('apps.Usuarios.usuarios.api.router')),
     
 ]
