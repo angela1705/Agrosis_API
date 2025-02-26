91d8c4d41b5d53fbe0f6ad69f773419be1648a23
David <Juandavides120@gmail.com>
2025-02-26 16:36:28 -0500

    creacion de los archivos vies.py routers.py y serializers.py para definir las vitas, las rutas y el serializador en cada app de IOT

---
+++ apps/Iot/datos_meteorologicos/api/routers.py
@@ -0,0 +1,10 @@
+from .views import Datos_metereologicosViewset
+from rest_framework.routers import DefaultRouter
+from django.urls import re_path
+from apps.Iot.datos_meteorologicos.api.consumer import DatosMeteorologicosConsumer
+Datos_metereologicosRouter = DefaultRouter()
+
+Datos_metereologicosRouter.register(prefix='datosmetereologicos',viewset=Datos_metereologicosViewset,basename='datosmetereologicos')
+
+
+
