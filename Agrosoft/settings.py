7a7d9b65fff15b500814c69dd728780dd03ae942
GIOVANYSTIVEN <steventu06@gmail.com>
2025-02-27 19:42:21 -0500

    ajustes para el react del login

--- Agrosoft/settings.py
+++ Agrosoft/settings.py
@@ -88,6 +88,7 @@ INSTALLED_APPS = [
     'apps.Cultivo.cosechas',
     'apps.Cultivo.lotes',
     'channels',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -98,6 +99,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'Agrosoft.urls'
@@ -205,3 +207,13 @@ AUTHENTICATION_BACKENDS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
